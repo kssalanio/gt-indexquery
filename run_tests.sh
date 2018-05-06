9e3f51f4c9ea0ccf2268c59edc97d356ea7c784f
Ken Salanio <kssalanio@gmail.com>
2018-05-07 01:57:08 +0800

    updated bash run script

--- run_tests.sh
+++ run_tests.sh
@@ -7,6 +7,14 @@ REPS=$4
 
 for (( i=${ISTART}; i<=${IEND}; i+=${INCREM}))
 do
-   CMD_STR="./sbt \"run ${REPS} tile_raster /mnt/extdisk/data/merged_tiles/victorias_merge_$i.tif /mnt/extdisk/dump/victorias_merge_$i\" &> logs/tiling/victorias_$i_run_${REPS}.log"
-   echo $CMD_STR
+   CMD_STR="./sbt \"run tile_raster ${REPS} /mnt/extdisk/data/merged_tiles/victorias_merge_$i.tif /mnt/extdisk/dump/victorias_merge_$i\" &> logs/1_tiling/victorias_$i_run_${REPS}.log"
+   echo "Running: $CMD_STR"
+   eval CMD_STR
+done
+
+for (( i=${ISTART}; i<=${IEND}; i+=${INCREM}))
+do
+   CMD_STR="./sbt \"run map_meta ${REPS} /mnt/extdisk/dump/victorias_merge_$i /home/spark/coverage/mindoro_coverage.shp\" &> logs/2_map_meta/victorias_$i_run_${REPS}.log"
+   echo "Running: $CMD_STR"
+   eval CMD_STR
 done
