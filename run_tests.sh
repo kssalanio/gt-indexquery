697b836e708a47276cbf1d786130dca7d6ae1321
Ken Salanio <kssalanio@gmail.com>
2018-05-07 02:00:59 +0800

    fixed missing $ sign

--- run_tests.sh
+++ run_tests.sh
@@ -7,14 +7,14 @@ REPS=$4
 
 for (( i=${ISTART}; i<=${IEND}; i+=${INCREM}))
 do
-   CMD_STR="./sbt \"run tile_raster ${REPS} /mnt/extdisk/data/merged_tiles/victorias_merge_$i.tif /mnt/extdisk/dump/victorias_merge_$i\" &> logs/1_tiling/victorias_$i_run_${REPS}.log"
+   CMD_STR="./sbt \"run tile_raster ${REPS} /mnt/extdisk/data/merged_tiles/victorias_merge_$i.tif /mnt/extdisk/dump/victorias_merge_$i\" &> logs/1_tiling/victorias_${i}_run_${REPS}.log"
    echo "Running: $CMD_STR"
    eval $CMD_STR
 done
 
 for (( i=${ISTART}; i<=${IEND}; i+=${INCREM}))
 do
-   CMD_STR="./sbt \"run map_meta ${REPS} /mnt/extdisk/dump/victorias_merge_$i /home/spark/coverage/mindoro_coverage.shp\" &> logs/2_map_meta/victorias_$i_run_${REPS}.log"
+   CMD_STR="./sbt \"run map_meta ${REPS} /mnt/extdisk/dump/victorias_merge_$i /home/spark/coverage/mindoro_coverage.shp\" &> logs/2_map_meta/victorias_${i}_run_${REPS}.log"
    echo "Running: $CMD_STR"
    eval $CMD_STR
 done
